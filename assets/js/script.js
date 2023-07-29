ac8f317c5d93b8b8e59fff73d8c4496cccfcd6cb
Kevin Ribas <ribaskevin@gmail.com>
2023-07-28 22:19:16 -0300

    responsive menu

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+/* Open and close mobile menu */
+
+const menuMobile = document.querySelector('.menu-mobile');
+const body = document.querySelector('body');
+
+menuMobile.addEventListener('click', () => {
+    menuMobile.classList.contains('bi-list')
+    ? menuMobile.classList.replace('bi-list', 'bi-x')
+    : menuMobile.classList.replace('bi-x', 'bi-list');
+    body.classList.toggle('menu-mobile-active');
+});
