e3e9bc259c5653969b60b6325d709bcfa4b511e2
Kevin Ribas <ribaskevin@gmail.com>
2023-08-01 18:09:24 -0300

    skills section

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,3 +9,19 @@ menuMobile.addEventListener('click', () => {
     : menuMobile.classList.replace('bi-x', 'bi-list');
     body.classList.toggle('menu-mobile-active');
 });
+
+/*  Closes the menu when an item is clicked  */
+
+const navItem = document.querySelectorAll('.nav-item');
+
+navItem.forEach(item => {
+    item.addEventListener('click', () => {
+        if (body.classList.contains('menu-mobile-active')) {
+            body.classList.remove('menu-mobile-active');
+            menuMobile.classList.replace('bi-x', 'bi-list');
+        }
+        const current = document.querySelector('.active');
+        current.classList.remove('active');
+        item.classList.add('active');
+    });
+});
